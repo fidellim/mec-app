26218c9230c123d4af3ad4d36445dfbbbfdd21c4
Fidel Lim <lim.fidel@gmail.com>
2026-05-20 09:03:12 +0400

    delete: about-timesheets artisan command

--- routes/console.php
+++ routes/console.php
@@ -4,10 +4,6 @@ use App\Models\AutomationSetting;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('about-timesheets', function () {
-    $this->info('Timesheet Management System Phase 1');
-});
-
 Schedule::command('timesheets:send-missing-reminders')
     ->mondays()
     ->at('07:00')
