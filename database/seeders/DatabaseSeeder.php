17c4c2a13f7af8807e12cc829767db1559089f84
Fidel Lim <lim.fidel@gmail.com>
2026-06-30 16:45:45 +0400

    feat: add leave settings seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Department;
 use App\Models\AutomationSetting;
-use App\Models\LeaveSetting;
 use App\Models\Project;
 use App\Models\TimesheetPeriod;
 use App\Models\User;
@@ -112,60 +111,7 @@ class DatabaseSeeder extends Seeder
             AutomationSetting::updateOrCreate(['key' => $key], $attributes);
         }
 
-        foreach ([
-            LeaveSetting::ANNUAL_LEAVE_DEFAULT_DAYS_UAE => [
-                'name' => 'UAE Annual Leave Default Days',
-                'description' => 'Default yearly L100 annual leave allowance for UAE employees. Unused days expire on December 31 and do not carry over.',
-                'decimal_value' => 22,
-            ],
-            LeaveSetting::ANNUAL_LEAVE_DEFAULT_DAYS_PH => [
-                'name' => 'Philippines Annual Leave Default Days',
-                'description' => 'Default yearly L100 annual leave allowance for Philippines employees. Unused days expire on December 31 and do not carry over.',
-                'decimal_value' => 5,
-            ],
-            LeaveSetting::SICK_LEAVE_DEFAULT_DAYS_UAE => [
-                'name' => 'UAE Sick Leave Default Days',
-                'description' => 'Default yearly L110 sick leave allowance for UAE employees. Unused days expire on December 31 and do not carry over.',
-                'decimal_value' => 15,
-            ],
-            LeaveSetting::SICK_LEAVE_DEFAULT_DAYS_PH => [
-                'name' => 'Philippines Sick Leave Default Days',
-                'description' => 'Default yearly L110 sick leave allowance for Philippines employees. Unused days expire on December 31 and do not carry over.',
-                'decimal_value' => 5,
-            ],
-            LeaveSetting::MATERNITY_LEAVE_DEFAULT_DAYS_UAE => [
-                'name' => 'UAE Maternity Leave Default Days',
-                'description' => 'Default L160 maternity leave policy allowance for UAE employees. Eligibility is reviewed manually.',
-                'decimal_value' => 60,
-            ],
-            LeaveSetting::MATERNITY_LEAVE_DEFAULT_DAYS_PH => [
-                'name' => 'Philippines Maternity Leave Default Days',
-                'description' => 'Default L160 maternity leave policy allowance for Philippines employees. Eligibility is reviewed manually.',
-                'decimal_value' => 60,
-            ],
-            LeaveSetting::PARENTAL_LEAVE_DEFAULT_DAYS_UAE => [
-                'name' => 'UAE Parental Leave Default Days',
-                'description' => 'Default L170 parental leave policy allowance for UAE employees. Eligibility is reviewed manually.',
-                'decimal_value' => 5,
-            ],
-            LeaveSetting::PARENTAL_LEAVE_DEFAULT_DAYS_PH => [
-                'name' => 'Philippines Parental Leave Default Days',
-                'description' => 'Default L170 parental leave policy allowance for Philippines employees. Eligibility is reviewed manually.',
-                'decimal_value' => 5,
-            ],
-            LeaveSetting::BEREAVEMENT_COMPASSIONATE_LEAVE_DEFAULT_DAYS_UAE => [
-                'name' => 'UAE Bereavement / Compassionate Leave Default Days',
-                'description' => 'Default L180 bereavement / compassionate leave policy allowance for UAE employees. Eligibility is reviewed manually.',
-                'decimal_value' => 8,
-            ],
-            LeaveSetting::BEREAVEMENT_COMPASSIONATE_LEAVE_DEFAULT_DAYS_PH => [
-                'name' => 'Philippines Bereavement / Compassionate Leave Default Days',
-                'description' => 'Default L180 bereavement / compassionate leave policy allowance for Philippines employees. Eligibility is reviewed manually.',
-                'decimal_value' => 8,
-            ],
-        ] as $key => $attributes) {
-            LeaveSetting::updateOrCreate(['key' => $key], $attributes);
-        }
+        $this->call(LeaveSettingSeeder::class);
 
         $start = Carbon::now()->startOfWeek();
         TimesheetPeriod::firstOrCreate(
